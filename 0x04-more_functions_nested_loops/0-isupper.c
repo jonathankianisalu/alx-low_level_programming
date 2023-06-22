4c8359d4441f995aa18883a1bd97ab201bcef225
Jonathan Kianisalu <jonathankianisalu62@gmail.com>
2023-06-22 13:46:10 -0700

    message

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _isupper - checks for uppercase character
- *@c: int
- *Return: 1 if upper, 0 other
- */
-
-int _isupper(int c)
+   * main - check the code for Holberton School students.
+    *
+     * Return: Always 0.
+      */
+int main(void)
 {
-char alpha;
-for (alpha = 'A'; alpha <= 'Z'; alpha++)
-{
-	if (c == alpha)
-		return (1);
-}
+	    char c;
 
-return (0);
+	        c = 'A';
+		    printf("%c: %d\n", c, _isupper(c));
+		        c = 'a';
+			    printf("%c: %d\n", c, _isupper(c));
+			        return (0);
+
 
